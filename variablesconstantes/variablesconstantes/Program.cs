3388c44e53eb21a507055e6f768f17d905881902
santiagocallem <santiago.callem@upb.edu.co>
2026-07-30 08:21:51 -0500

    Primer Commit

---
+++ variablesconstantes/variablesconstantes/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace variablesconstantes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Aquí va el código fuente
+            //Variables
+
+            string nombre = "Santiago";
+            Console.WriteLine("Bienvenido "+ nombre+" al curso Fundamentos de Programación");
+        }
+    }
+}
