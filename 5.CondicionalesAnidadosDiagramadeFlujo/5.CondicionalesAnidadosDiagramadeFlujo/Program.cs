a050ae27253d87f7c97fc72da8652af4d57621e5
santiagocallem <santiago.callem@upb.edu.co>
2026-08-11 09:44:56 -0500

    Condicionales Anidados y Diagramas de Flujo

---
+++ 5.CondicionalesAnidadosDiagramadeFlujo/5.CondicionalesAnidadosDiagramadeFlujo/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Diagnostics.Eventing.Reader;
+
+namespace _5.CondicionalesAnidadosDiagramadeFlujo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Condicionales Anidados - Diagrama de flujo
+            //b.    Diseñar un algoritmo que lea tres números A, B, C y visualice en pantalla el valor más grande. El usuario debe ingresar tres valores diferentes.
+            int num1 = 0;
+            int num2 = 0;
+            int num3 = 0;
+            Console.WriteLine("Debe ingresar 3 valores diferentes");
+            Console.WriteLine("Ingrese el valor del número 1");
+            num1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese el valor del número 2");
+            num2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese el valor del número 3");
+            num3 = int.Parse(Console.ReadLine());
+
+            if (num1 > num2)
+            {   //verdad
+                if (num1 > num3)
+                {
+                    Console.WriteLine("El número 1 es el mayor");
+                }
+                else
+                {
+                    Console.WriteLine("El número 3 es el mayor");
+                }
+            }        
+            else
+            {  //Falsa
+               if (num2 > num3)
+                {
+                    Console.WriteLine("El número 2 es el mayor");
+                }
+                else
+                {
+                    Console.WriteLine("El número 3 es el mayor");
+                }
+            }
+    }
+}
