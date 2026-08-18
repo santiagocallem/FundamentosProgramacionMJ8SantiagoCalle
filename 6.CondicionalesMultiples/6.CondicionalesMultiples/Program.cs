b9c9da6c0c981db2b30fc1d16fd5b546d8002c8e
santiagocallem <santiago.callem@upb.edu.co>
2026-08-18 09:47:42 -0500

    6. Condicionales Multiples

---
+++ 6.CondicionalesMultiples/6.CondicionalesMultiples/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace _6.CondicionalesMultiples
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Condicionales Múltiples
+            //Crear un algoritmo que muestre por pantalla un menú con 5 opciones. y según la opción elegida del menú, muestre un mesaje por pantalla donde se muestre la opción elegida, en caso de que se seleccione una opción que no estpa en el menú mostrar un mensaje de error.
+            int respuesta = 0;
+            Console.WriteLine("---------------Menú---------------");
+            Console.WriteLine("1. Opción 1        2. Opción 2 ");
+            Console.WriteLine("3. Opción 3        4. Opción 4 ");
+            Console.WriteLine("5. Opción 5");
+            Console.WriteLine"(-----------------------------------");
+            Console.WriteLine("Elija una opción del menú: ");
+            respuesta = int.Parse(Console.ReadLine());
+
+            switch (respuesta)
+            {
+                case 1:
+                    Console.WriteLine("Eligió opción 1");
+                    break;
+                case 2:
+                    Console.WriteLine("Eligió opción 2");
+                    break;
+                case 3:
+                    Console.WriteLine("Eligió opción 3");
+                    break;
+                case 4:
+                    Console.WriteLine("Eligió opción 4");
+                    break;
+                case 5:
+                    Console.WriteLine("Eligió opción 5");
+                    break;
+                default:
+                    Console.WriteLine("Eliga una opción valida");
+                    break;
+            }
+
+        }
+    }
+}
