e87d7dea7d55ab2479cf9b1903fb87294950a0ed
santiagocallem <santiago.callem@upb.edu.co>
2026-08-27 09:31:18 -0500

    9. Ciclo While #2

---
+++ 9.CicloWhile2/9.CicloWhile2/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _9.CicloWhile2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int cantidadNumeros = 0;
+            int numero = 0;
+            int contador = 0;
+            int contadorPositivos = 0;
+            int contadorNegativos = 0;
+            int contadorCeros = 0;
+            Console.WriteLine("Ingrese la cantidad de números a evaluar:");
+            cantidadNumeros=int.Parse(Console.ReadLine());
+
+            while (cantidadNumeros>=contador)
+            {
+                contador++;
+                Console.WriteLine("Ingrese el número a evaluar:");
+                numero=int.Parse(Console.ReadLine());
+
+                if (numero < 0)
+                {
+                    contadorNegativos++;
+                }
+                else
+                {
+                    if (numero > 0)
+                    {
+                        contadorPositivos++;
+                    }
+                    else
+                    {
+                        contadorCeros++;
+                    }
+                }
+            }
+            Console.WriteLine($"Cantidad positivos: {contadorPositivos}, cantidad negativos: {contadorNegativos}, cantidad ceros: {contadorCeros}");
+        }
+    }
+}
