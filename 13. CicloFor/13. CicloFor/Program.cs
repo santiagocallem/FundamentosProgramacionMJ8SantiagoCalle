8b8ef08070650ec8fc4f7153c8b5337c0e8dc9fd
santiagocallem <santiago.callem@upb.edu.co>
2026-09-03 09:27:59 -0500

    13. Ciclo For

---
+++ 13. CicloFor/13. CicloFor/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _13.CicloFor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Ciclo For
+            int acumulador = 0;
+
+            for (int contador = 1; contador <= 5; contador ++)
+            {
+                acumulador += contador;
+            }
+            Console.WriteLine($"La suma de los cinco primeros números es: " + acumulador);
+        }
+    }
+}
---
+++ 13. CicloFor/13. CicloFor/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// La información general de un ensamblado se controla mediante el siguiente 
+// conjunto de atributos. Cambie estos valores de atributo para modificar la información
+// asociada a un ensamblado.
+[assembly: AssemblyTitle("13. CicloFor")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("13. CicloFor")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Si establece ComVisible en false, los tipos de este ensamblado no estarán visibles 
+// para los componentes COM.  Si es necesario obtener acceso a un tipo en este ensamblado desde 
+// COM, establezca el atributo ComVisible en true en este tipo.
+[assembly: ComVisible(false)]
+
+// El siguiente GUID sirve como id. de typelib si este proyecto se expone a COM.
+[assembly: Guid("3b94fa42-21f8-4950-9ec4-d68e47f3c02d")]
+
+// La información de versión de un ensamblado consta de los cuatro valores siguientes:
+//
+//      Versión principal
+//      Versión secundaria
+//      Número de compilación
+//      Revisión
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
